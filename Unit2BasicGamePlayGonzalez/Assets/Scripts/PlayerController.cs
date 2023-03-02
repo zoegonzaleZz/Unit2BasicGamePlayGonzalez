5b83dabb0a59d58a54afd88e6c4d37f4dd2e7bff
zoegonzaleZz <117196164+zoegonzaleZz@users.noreply.github.com>
2023-03-02 09:53:43 -0800

    First Day
    Did tutorial 1

---
+++ Unit2BasicGamePlayGonzalez/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float leftBoundary = -10.0f;
+    public float rightBoundary = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < leftBoundary)
+        {
+            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x > rightBoundary)
+        {
+            transform.position = new Vector3(10, transform.position.y, transform.position.z);
+        }
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+    }
+}
