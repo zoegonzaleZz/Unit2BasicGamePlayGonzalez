861e72dbab98a786d6784b9bea9f5b7e4fe1a5bb
zoegonzaleZz <117196164+zoegonzaleZz@users.noreply.github.com>
2023-03-06 09:56:00 -0800

    Tut. 2
    Finished Food Fight

---
+++ Unit2BasicGamePlayGonzalez/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+
+    public GameObject[] animalPrefab;
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
