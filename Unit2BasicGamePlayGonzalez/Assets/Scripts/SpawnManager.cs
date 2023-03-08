0331ea03f1aa0a27988b6e14535934e1e04d78a9
zoegonzaleZz <117196164+zoegonzaleZz@users.noreply.github.com>
2023-03-08 09:51:12 -0800

    Did 2.4
    Did spawn point and am starting food eating

--- Unit2BasicGamePlayGonzalez/Assets/Scripts/SpawnManager.cs
+++ Unit2BasicGamePlayGonzalez/Assets/Scripts/SpawnManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
 
-    public GameObject[] animalPrefab;
+    public GameObject[] animalPrefabs;
     private float spawnRangeX = 20;
     private float spawnPosZ = 20;
     private float startDelay = 2;
@@ -21,5 +21,13 @@ public class SpawnManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void SpawnRandomAnimal()
+    {
+        // Randomly generate animal index and spam position
+        Vector3 spawnPos = new Vector3(Random.Range (-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
     }
 }
